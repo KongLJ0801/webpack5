f0e578d3a842a96cb0bee8b038fe983cedcf8490
jack <13522682259@163.com>
2021-06-04 17:01:39 +0800

    webpack 03

--- 03.打包样式资源/webpack.config.js
+++ 03.打包样式资源/webpack.config.js
@@ -1,5 +1,18 @@
+const { resolve } = require('path')
 module.exports = {
-    //入口
+    //  入口
     entry: "./src/index.js",
-    //
+    //  输出
+    output: {
+        //  输出文件名
+        filename: 'build.js',
+        //  输出路径
+        path: resolve(__dirname, 'build')
+    },
+    // loader   的设置
+    module:{
+        
+    }
+
+
 };
